432524047bde36fe47526626635bf85209113d4f
Sushant Verma <delhi.sushantverma@gmail.com>
2020-07-31 20:30:26 +0530

    basic routing added and 404 page added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Routes from "./routes/routes";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class ReactTest extends React.Component {
+
+  componentDidMount() {
+    //do nothing
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes />
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<ReactTest />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import App from "../App";
+import NotFoundPage from "../NotFoundPage";
+
+export default () =>
+  <Switch>
+    <Route path="/" exact component={App} />
+    {/* <Route path="/login" exact component={SignInSide} /> */}
+    {/*<Route path="/blog" exact component={BlogPage} />*/}
+    { /* Finally, catch all unmatched routes */}
+    <Route component={NotFoundPage} />
+  </Switch>;
