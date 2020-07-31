d8409198c3e605bcad126d70c02bd59720f2b55f
Sushant Verma <delhi.sushantverma@gmail.com>
2020-07-31 20:16:23 +0530

    new test file added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and stay home.
         </p>
         <a
           className="App-link"
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Test />
     </div>
   );
 }
---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <p className="App-header">
+        Hey guys, What's up !!
+    </p>
+  );
+}
+
+export default Test;
